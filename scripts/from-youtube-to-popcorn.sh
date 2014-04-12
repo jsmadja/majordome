6a6c9ce179cb0b144c64c2e01881022174f569c0
jsmadja <julien.smadja@gmail.com>
2014-04-12 12:36:25 +0200

    Youtube

--- scripts/from-youtube-to-popcorn.sh
+++ scripts/from-youtube-to-popcorn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir store
 cd store
 youtube-dl $1 --max-quality=22
 lftp nmt:1234@kaddath.hd.free.fr -e "cd SATA_DISK; cd Download; mput *; quit;"
--- server-routes.js
+++ server-routes.js
@@ -3,13 +3,8 @@ var _ = require('underscore');
 var sys = require('sys')
 var exec = require('child_process').exec;
 
-
-function puts(error, stdout, stderr) {
-    sys.puts(stdout)
-}
-
 exports.youtube = function (req, res) {
-    var command = "scripts/from-youtube-to-popcorn.sh https://www.youtube.com/" + req.params;
+    var command = "scripts/from-youtube-to-popcorn.sh http://www.youtube.com/" + req.params;
     console.log(command);
     exec(command, function (error, stdout, stderr) {
         res.send({out: stdout, err: stderr, error: error});
