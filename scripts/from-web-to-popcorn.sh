ff41653b04bd1567e914833fa8bf0395b4778a60
jsmadja <julien.smadja@gmail.com>
2014-04-14 19:44:40 +0200

    From Web To Popcorn

--- scripts/from-web-to-popcorn.sh
+++ scripts/from-web-to-popcorn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dir=$(date +%s)
 cd web_store
-echo "wget $1 --directory-prefix=$dir"
+wget $1 --directory-prefix=$dir
 lftp nmt:1234@kaddath.hd.free.fr -e "cd SATA_DISK; cd Download; mput *; quit;"
 rm -rf *
 cd ..
